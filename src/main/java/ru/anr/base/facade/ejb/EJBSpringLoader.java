17a1576ef2c1fdf30424978983317e07b40389f4
Alexey Romanchuk <alexey.p.romanchuk@gmail.com>
2015-06-10 15:10:39 +0000

    Added support of auto-started cycled queues

--- src/main/java/ru/anr/base/facade/ejb/EJBSpringLoader.java
+++ src/main/java/ru/anr/base/facade/ejb/EJBSpringLoader.java
@@ -15,13 +15,21 @@
  */
 package ru.anr.base.facade.ejb;
 
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
 import javax.interceptor.Interceptors;
+import javax.jms.Destination;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.jms.core.JmsOperations;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+
+import ru.anr.base.BaseSpringParent;
+import ru.anr.base.facade.ejb.mdb.BaseEventKeyStrategy;
 
 /**
  * Singleton EJB, which loads spring context via {@link SpringEJBInterceptor}
@@ -35,7 +43,7 @@ import org.springframework.context.ApplicationContext;
  *
  */
 @Interceptors(SpringEJBInterceptor.class)
-public class EJBSpringLoader {
+public class EJBSpringLoader extends BaseSpringParent {
 
     /**
      * Logger
@@ -48,10 +56,60 @@ public class EJBSpringLoader {
     private final EJBContextHolder holder = new EJBContextHolder();
 
     /**
-     * An attempt to inject context
+     * Reference to {@link JmsOperations}
      */
-    @Autowired
-    private ApplicationContext ctx;
+    @Autowired(required = false)
+    private JmsOperations jms;
+
+    /**
+     * Initialization of a queue ( JmsOperation specified above must be set).
+     * 
+     * @param queueBean
+     *            The queue to be initialized ( specified as a bean in the
+     *            context)
+     * @param messageKey
+     *            Message key
+     * @param headerPairs
+     *            Pairs name/value for additional headers
+     * @param text
+     *            Text inside of the message (to understand what it is the
+     *            message for)
+     */
+    protected void initQueue(String queueBean, String messageKey, String text, Object... headerPairs) {
+
+        Map<String, Object> hh = toMap(headerPairs);
+        hh.put(BaseEventKeyStrategy.TYPE_KEY, messageKey);
+
+        Message<String> msg = new GenericMessage<String>(text, hh);
+
+        Destination queue = bean(queueBean, Destination.class);
+        jms.convertAndSend(queue, msg);
+
+        logger.info("Queue: {} initialized", messageKey);
+    }
+
+    /**
+     * The map which stores information about the queues to be initialized
+     */
+    private Map<String, Object[]> queueMap = toMap();
+
+    /**
+     * Adding queues for further initialization. Real initialization will be
+     * available in 'production' mode only.
+     * 
+     * @param queueBean
+     *            The name of queue bean
+     * @param messageKey
+     *            The key for message identification
+     * @param headerPairs
+     *            Pairs 'name'/'value' for additional headers
+     * @param body
+     *            Message body
+     */
+    protected void addQueue(String queueBean, String messageKey, String body, Object... headerPairs) {
+
+        queueMap.put(queueBean, new Object[]{ messageKey, body, headerPairs });
+    }
 
     /**
      * Initialization
@@ -59,6 +117,14 @@ public class EJBSpringLoader {
     @PostConstruct
     public void init() {
 
-        logger.info("Holder: {}, context loaded: {}", holder, ctx);
+        logger.info("Holder: {}, profiles: {}", holder, getProfiles());
+
+        if (isProdMode()) {
+            queueMap.forEach((q, a) -> {
+                initQueue(q.toString(), a[0].toString(), a[1].toString(), (Object[]) a[3]);
+            });
+        } else {
+            logger.info("Don't initialize the cycled queues when not in production mode");
+        }
     }
 }
